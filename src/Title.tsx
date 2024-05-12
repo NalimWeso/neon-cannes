7087214acc134ed1f073875d34abea3957aa2495
Milan Wesołowski <milanweso@gmail.com>
2024-05-12 10:00:00 +0200

    Implement font

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,7 +1,11 @@
 function Title() {
     return (
         <>
-            <h1 className="text-4xl my-3 text-center">Neon Cannes</h1>
+            <div className="text-lg m-2 leading-6">
+                <p>01.01: Judgment at Nuremberg (1961)</p>
+                <p>01.02: Godzilla (1998)</p>
+                <p>01.03: Incendies (2010)</p>
+            </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kannada: ["Anek Kannada", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
