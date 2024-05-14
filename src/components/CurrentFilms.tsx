91da544884f45e4dce21b99dd057c74bfc94385d
Milan Wesołowski <milanweso@gmail.com>
2024-05-14 17:16:58 +0200

    Implement Radix UI to CurrentFilms

--- src/components/CurrentFilms.tsx
+++ src/components/CurrentFilms.tsx
@@ -1,13 +1,33 @@
-function CurrentFilms() {
+import { Text, Badge, Button } from '@radix-ui/themes';
+
+export default function CurrentFilms() {
     return (
         <>
-            <div className="text-base p-1 leading-6">
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer"><span className="text-amber-500 font-bold">01.01:</span> Judgment at Nuremberg (1961)</p><br />
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer"><span className="text-amber-500 font-bold">02.01:</span> Godzilla (1998)</p><br />
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer"><span className="text-amber-500 font-bold">03.01:</span> Incendies (2010)</p><br />
+            <div className="text-base px-2 py-1 leading-6">
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
+                    <Text size="2">&#160;Judgment at Nuremberg&#160;</Text>
+                    <Badge size="2" color="orange" className="inline-block">1961</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
+
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
+                    <Text size="2">&#160;Godzilla&#160;</Text>
+                    <Badge size="2" color="orange" className="inline-block">1998</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
+
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
+                    <Text size="2">&#160;Incendies&#160;</Text>
+                    <Badge size="2" color="orange" className="inline-block">2010</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
             </div>
         </>
     )
 }
-
-export default CurrentFilms
