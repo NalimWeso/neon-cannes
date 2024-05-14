1f935eee1ad3f97e6482a102c451951bb6f55164
Milan Wesołowski <milanweso@gmail.com>
2024-05-14 17:37:33 +0200

    Modify content

--- src/components/CurrentFilms.tsx
+++ src/components/CurrentFilms.tsx
@@ -1,4 +1,4 @@
-import { Text, Badge, Button } from '@radix-ui/themes';
+import { Badge, Button } from '@radix-ui/themes';
 
 export default function CurrentFilms() {
     return (
@@ -6,24 +6,21 @@ export default function CurrentFilms() {
             <div className="text-base px-2 py-1 leading-6">
                 <div className="cursor-default my-1">
                     <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
-                    <Text size="2">&#160;Judgment at Nuremberg&#160;</Text>
-                    <Badge size="2" color="orange" className="inline-block">1961</Badge>
+                    <Badge size="2" color="orange" className="ml-1 inline-block">Judgment at Nuremberg (1961)</Badge>
                     <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
                     <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
                 </div>
 
                 <div className="cursor-default my-1">
-                    <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
-                    <Text size="2">&#160;Godzilla&#160;</Text>
-                    <Badge size="2" color="orange" className="inline-block">1998</Badge>
+                    <Badge size="2" color="orange" className="text-amber-500">02.01</Badge>
+                    <Badge size="2" color="orange" className="ml-1 inline-block">Godzilla (1998)</Badge>
                     <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
                     <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
                 </div>
 
                 <div className="cursor-default my-1">
-                    <Badge size="2" color="orange" className="text-amber-500">01.01</Badge>
-                    <Text size="2">&#160;Incendies&#160;</Text>
-                    <Badge size="2" color="orange" className="inline-block">2010</Badge>
+                    <Badge size="2" color="orange" className="text-amber-500">03.01</Badge>
+                    <Badge size="2" color="orange" className="ml-1 inline-block">Incendies (2010)</Badge>
                     <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
                     <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
                 </div>
--- src/components/FutureFilms.tsx
+++ src/components/FutureFilms.tsx
@@ -1,12 +1,28 @@
-function FutureFilms() {
+import { Badge, Button } from '@radix-ui/themes';
+
+export default function FutureFilms() {
     return (
         <>
             <div className="text-base p-1 leading-6">
                 <h4 className="font-bold text-amber-500 my-1">Great Category:</h4>
 
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer">A Bittersweet Life (2005)</p><br />
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer">The Bridge on the River Kwai (1957)</p><br />
-                <p className="inline-block hover:underline decoration-amber-500 cursor-pointer">Kiss the Girls and Make Them Die (1966)</p><br />
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="ml-1 inline-block">A Bittersweet Life (2005)</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
+
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="ml-1 inline-block">The Bridge on the River Kwai (1957)</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
+
+                <div className="cursor-default my-1">
+                    <Badge size="2" color="orange" className="ml-1 inline-block">Kiss the Girls and Make Them Die (1966)</Badge>
+                    <Button size="1" color="jade" variant="soft" className="mx-1 mt-0.5 cursor-pointer">Copy</Button>
+                    <Button size="1" color="cyan" variant="soft" className="mt-0.5 cursor-pointer">Edit</Button>
+                </div>
             </div>
 
             <div className="text-base p-1 leading-6">
@@ -19,5 +35,3 @@ function FutureFilms() {
         </>
     )
 }
-
-export default FutureFilms
