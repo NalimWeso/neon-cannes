72f3becd694cce7e979100cda1d1be2b6c110725
Milan Wesołowski <milanweso@gmail.com>
2024-05-16 08:30:44 +0200

    Implement minor changes

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -7,18 +7,18 @@ export default function TitleBar() {
 
     return (
         <>
-            <div className="h-8 w-full bg-titlebar flex justify-between items-center">
+            <div className="h-7 w-full bg-titlebar flex justify-between items-center">
                 <div className='w-full draggable'>
-                    <Text className='text-amber-500 text-center text-xl font-bold mt-1.5 ml-1'>Neon Cannes</Text>
+                    <Text className='text-amber-500 text-center font-bold mt-1.5 ml-1'>Neon Cannes</Text>
                 </div>
 
                 <div className='flex'>
-                    <Button size="1" radius="none" variant="solid" className=' bg-titlebar hover:bg-amber-600 transition duration-50 h-9 w-12 -mt-1'
+                    <Button size="1" radius="none" variant="solid" className=' bg-titlebar hover:bg-amber-600 transition duration-50 h-8 w-12 -mt-1'
                         onClick={() => ipc.send("minimize")}>
                         <MinusIcon />
                     </Button>
 
-                    <Button size="1" radius="none" variant="solid" className=' bg-titlebar hover:bg-red-800 transition duration-50 h-9 w-12 -mt-1 -mr-0.5'
+                    <Button size="1" radius="none" variant="solid" className=' bg-titlebar hover:bg-red-800 transition duration-50 h-8 w-12 -mt-1 -mr-0.5'
                         onClick={() => ipc.send("close")}>
                         <Cross1Icon />
                     </Button>
