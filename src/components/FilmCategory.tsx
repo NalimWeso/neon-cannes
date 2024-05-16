0125e06e11ab2a93d11b59d38a818715965d8c8e
Milan Wesołowski <milanweso@gmail.com>
2024-05-16 14:50:41 +0200

    Move film categories to another component

---
+++ src/components/FilmCategory.tsx
@@ -0,0 +1,43 @@
+import { Pencil2Icon, CopyIcon, CheckIcon } from '@radix-ui/react-icons';
+import { Badge, Button } from '@radix-ui/themes';
+import { useState } from 'react';
+import { Film } from './FilmType';
+
+interface Props {
+    films: Film[];
+    category: string;
+}
+
+export default function FilmCategory({ films, category }: Props) {
+    const [copy, setCopy] = useState("");
+
+    return (
+        <div className="pt-4">
+            <Badge size="3" color='orange' className="font-bold text-amber-500">{category}</Badge>
+            <Button size="1" color="orange" variant="soft" className="text-amber-500 text-xs ml-1 py-3.5 transition cursor-pointer"><Pencil2Icon /></Button>
+
+            {films.filter(film => film.category === category).map((film) => (
+                <div key={film.id} className="my-1 flex">
+                    <Badge size="2" color={film.season ? "teal" : "orange"} className="inline-block">
+                        {film.title} ({film.year}{film.yearEnd && ` - ${film.yearEnd}`}) {film.season && `– ${film.season}`}
+                    </Badge>
+
+                    <Button size="1" color="jade" variant="soft" className="text-lime-700 hover:text-lime-600 transition mx-1 cursor-pointer">
+                        <Pencil2Icon />
+                    </Button>
+
+                    <Button size="1" color="cyan" variant="soft" className="text-cyan-700 hover:text-cyan-600 transition cursor-pointer"
+                        onClick={() => {
+                            navigator.clipboard.writeText(film.title);
+                            setCopy(film.id);
+                            setTimeout(() => setCopy(""), 300);
+                        }}>
+                        <CopyIcon />
+                    </Button>
+
+                    {copy === film.id && <Badge size="2" color="cyan" className="text-cyan-600 ml-1 h-6 w-8 justify-center"><CheckIcon /></Badge>}
+                </div>
+            ))}
+        </div>
+    );
+}
--- src/components/FutureFilms.tsx
+++ src/components/FutureFilms.tsx
@@ -1,12 +1,9 @@
-import { Pencil2Icon, CopyIcon, CheckIcon } from '@radix-ui/react-icons';
-import { Badge, Button } from '@radix-ui/themes';
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { v4 as uuid } from 'uuid';
 import { Film } from './FilmType';
+import FilmCategory from './FilmCategory';
 
 export default function FutureFilms() {
-    const [copy, setCopy] = useState("");
-
     const films: Film[] = useMemo(() => [
         { id: uuid(), title: "A Bittersweet Life", year: 2005, category: "Tier I" },
         { id: uuid(), title: "The Bridge on the River Kwai", year: 1957, category: "Tier I" },
@@ -18,61 +15,8 @@ export default function FutureFilms() {
 
     return (
         <div className='px-2'>
-            <div className="pt-4">
-                <Badge size="3" color='orange' className="font-bold text-amber-500">Tier I</Badge>
-                <Button size="1" color="orange" variant="soft" className="text-amber-500 text-xs ml-1 py-3.5 transition cursor-pointer"><Pencil2Icon /></Button>
-
-                {films.filter(film => film.category === "Tier I").map((film) => (
-                    <div key={film.id} className="my-1 flex">
-                        <Badge size="2" color={film.season ? "teal" : "orange"} className="inline-block">
-                            {film.title} ({film.year}{film.yearEnd && ` - ${film.yearEnd}`}) {film.season && `– ${film.season}`}
-                        </Badge>
-
-                        <Button size="1" color="jade" variant="soft" className="text-lime-700 hover:text-lime-600 transition mx-1 cursor-pointer">
-                            <Pencil2Icon />
-                        </Button>
-
-                        <Button size="1" color="cyan" variant="soft" className="text-cyan-700 hover:text-cyan-600 transition cursor-pointer"
-                            onClick={() => {
-                                navigator.clipboard.writeText(film.title);
-                                setCopy(film.id);
-                                setTimeout(() => setCopy(""), 300);
-                            }}>
-                            <CopyIcon />
-                        </Button>
-
-                        {copy === film.id && <Badge size="2" color="cyan" className="text-cyan-600 ml-1 h-6 w-8 justify-center"><CheckIcon /></Badge>}
-                    </div>
-                ))}
-            </div>
-
-            <div className="pt-4">
-                <Badge size="3" color='orange' className="font-bold text-amber-500">Animation</Badge>
-                <Button size="1" color="orange" variant="soft" className="text-amber-500 text-xs ml-1 py-3.5 transition cursor-pointer"><Pencil2Icon /></Button>
-
-                {films.filter(film => film.category === "Animation").map((film) => (
-                    <div key={film.id} className="my-1 flex">
-                        <Badge size="2" color={film.season ? "teal" : "orange"} className="inline-block">
-                            {film.title} ({film.year}{film.yearEnd && ` - ${film.yearEnd}`}) {film.season && `– ${film.season}`}
-                        </Badge>
-
-                        <Button size="1" color="jade" variant="soft" className="text-lime-700 hover:text-lime-600 transition mx-1 cursor-pointer">
-                            <Pencil2Icon />
-                        </Button>
-
-                        <Button size="1" color="cyan" variant="soft" className="text-cyan-700 hover:text-cyan-600 transition cursor-pointer"
-                            onClick={() => {
-                                navigator.clipboard.writeText(film.title);
-                                setCopy(film.id);
-                                setTimeout(() => setCopy(""), 300);
-                            }}>
-                            <CopyIcon />
-                        </Button>
-
-                        {copy === film.id && <Badge size="2" color="cyan" className="text-cyan-600 ml-1 h-6 w-8 justify-center"><CheckIcon /></Badge>}
-                    </div>
-                ))}
-            </div>
+            <FilmCategory films={films} category="Tier I" />
+            <FilmCategory films={films} category="Animation" />
         </div>
-    )
+    );
 }
