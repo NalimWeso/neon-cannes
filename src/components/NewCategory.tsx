f316612f269bd241eff233e0db57bc922186a668
Milan Wesołowski <milanweso@gmail.com>
2024-05-17 21:35:51 +0200

    Update NewCategory button

--- src/components/NewCategory.tsx
+++ src/components/NewCategory.tsx
@@ -3,7 +3,7 @@ import { Button } from '@radix-ui/themes';
 
 export default function NewCategory() {
     return (
-        <div className="ml-1 pt-6 pb-2">
+        <div className="mb-1 ml-1 pt-6">
             <Button size="1" color="orange" variant="soft" className="text-amber-500 text-sm font-bold px-3 py-3.5 rounded transition cursor-pointer">
                 New Category<RocketIcon className='ml-1' />
             </Button>
