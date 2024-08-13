99a8d29525417ecd607d077c05ab8c50d34da9db
Milan Wesołowski <milanweso@gmail.com>
2024-08-13 21:42:45 +0200

    Create DeleteContent

---
+++ src/components/utils/DeleteContent.tsx
@@ -0,0 +1,44 @@
+import { ipcRenderer } from 'electron';
+
+export default function DeleteContent(id: string, isCategory: boolean, data: any[]) {
+    let updatedData;
+
+    if (isCategory) {
+        const itemToDelete = data.find(item => item.id === id);
+        const positionToDelete = itemToDelete?.position;
+
+        if (positionToDelete !== undefined) {
+            updatedData = data
+                .filter(item => item.id !== id)
+                .map(item => {
+                    if (item.position > positionToDelete) {
+                        return { ...item, position: item.position - 1 };
+                    }
+                    return item;
+                });
+        }
+    } else {
+        updatedData = data.map(category => {
+            if (category.films) {
+                const updatedFilms = category.films.filter(film => film.id !== id);
+
+                if (updatedFilms.length < category.films.length) {
+                    const filmToDelete = category.films.find(film => film.id === id);
+                    if (filmToDelete?.index !== null && filmToDelete?.index !== undefined) {
+                        updatedFilms.forEach((film, index) => {
+                            film.index = index;
+                        });
+                    }
+                }
+
+                return {
+                    ...category,
+                    films: updatedFilms,
+                };
+            }
+            return category;
+        });
+    }
+
+    ipcRenderer.invoke('write-json', updatedData);
+}
